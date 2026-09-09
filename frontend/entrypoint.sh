2dc3dedc3f6a2a83a9e27847e3e4414d643ca802
Abdelrhman <Abdelrhman@Abdo-pc.com>
2026-09-09 20:11:58 +0300

    Configure environment variables for Nginx template in entrypoint script

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
+PORT="${PORT:-80}"
+BACKEND_URL="${BACKEND_URL:-http://backend:8000}"
+export PORT BACKEND_URL
 envsubst '${PORT} ${BACKEND_URL}' < /etc/nginx/templates/nginx.conf.template > /etc/nginx/conf.d/default.conf
 exec nginx -g 'daemon off;'
