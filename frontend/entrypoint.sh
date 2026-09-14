8f99064f7bd033b98bae9978a7fa37ee11941225
Abdelrhman <Abdelrhman@Abdo-pc.com>
2026-09-14 18:21:11 +0300

    Fix Railway admin proxy target

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 PORT="${PORT:-80}"
-BACKEND_URL="${BACKEND_URL:-http://backend:8000}"
+BACKEND_URL="${BACKEND_URL:-https://arabic-home-system-backend-6tpv-production.up.railway.app}"
 export PORT BACKEND_URL
 envsubst '${PORT} ${BACKEND_URL}' < /etc/nginx/templates/nginx.conf.template > /etc/nginx/conf.d/default.conf
 exec nginx -g 'daemon off;'
